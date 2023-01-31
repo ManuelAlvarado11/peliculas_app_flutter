6c6a7b0212a6ea0d240b5ba7fee90fd3aa745296
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-01-31 14:24:24 -0600

    :tada: Primeros Screen en la App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,16 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Peliculas App',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => const HomeScreen(),
+          'details': (_) => const DetailsScreen(),
+        });
   }
 }
