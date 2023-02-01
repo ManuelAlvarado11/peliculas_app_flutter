447bf2f466984d81567b0b8b27710e1ec6ca6dd8
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-02-01 15:09:29 -0600

    :rocket:Implementar Swiper - Carrusel de tarjetas

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class CardSwipper extends StatelessWidget {
   const CardSwipper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    final size = MediaQuery.of(context).size;
+
+    return SizedBox(
       width: double.infinity,
-      height: 500,
-      color: Colors.black,
+      height: size.height * 0.5,
+      child: Swiper(
+          itemCount: 10,
+          layout: SwiperLayout.STACK,
+          itemWidth: size.width * 0.6,
+          itemHeight: size.height * 0.4,
+          itemBuilder: (_, index) {
+            return GestureDetector(
+              onTap: () =>
+                  Navigator.pushNamed(context, 'details', arguments: 'movie'),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: const FadeInImage(
+                  fit: BoxFit.cover,
+                  placeholder: AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage('https://via.placeholder.com/300x400'),
+                ),
+              ),
+            );
+          }),
     );
   }
 }
