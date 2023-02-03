e77ef6b54673e7aa3f18741db1b86bdde9d823f4
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-02-03 09:15:33 -0600

    :sparkles: Add MovieDetails Screen (Slivers)

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,8 +5,114 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('DetailsScreen')),
+    return Scaffold(
+      // Crea efectos de desplazamiento personalizados usando astillas(slivers)
+      body: CustomScrollView(
+        slivers: [
+          const _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const _PostAndTitle(),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// Widget de AppBar Personalizado
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Widget AppBar dinamico
+    return SliverAppBar(
+      backgroundColor: Colors.blue,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      // Widget que se apila detrás de la barra de herramientas
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        // Titulo del FlexibleSpace
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black45,
+          child: const Text(
+            'movie.name',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        // Fondo del FlexibleSpace
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PostAndTitle extends StatelessWidget {
+  const _PostAndTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 5),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: const FadeInImage(
+              height: 200,
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'original.title',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.star_border_sharp,
+                    size: 20,
+                    color: Colors.grey,
+                  ),
+                  const SizedBox(width: 5),
+                  Text(
+                    'movie.puntaje',
+                    style: textTheme.caption,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
